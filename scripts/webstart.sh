9577ee53c4ef65b5bed625a494e1a692a7cd6151
munnotubbel <munnotubbel@tubbel-top.lan>
2024-10-14 09:24:01 +0200

    feat: enable webstart.sh to work without git, switch to debug build image

--- scripts/webstart.sh
+++ scripts/webstart.sh
@@ -15,40 +15,50 @@ print_fancy() {
     echo -e "${color_code}${prefix} ${message}\033[0m"
 }
 
-REPO_URL="https://github.com/munnotubbel/ententeich.git"
+REPO_URL="https://github.com/munnotubbel/ententeich"
+ZIP_URL="${REPO_URL}/archive/refs/heads/main.zip"
 TARGET_DIR="ententeich"
+ZIP_FILE="ententeich.zip"
 
-print_fancy "Cloning repository..." "yellow" "[INFO]"
-git clone $REPO_URL $TARGET_DIR
-
-if [ $? -eq 0 ]; then
-    print_fancy "Repository cloned successfully." "green" "[SUCCESS]"
+print_fancy "Downloading repository as ZIP..." "yellow" "[INFO]"
+if curl -L -o "$ZIP_FILE" "$ZIP_URL"; then
+    print_fancy "Repository ZIP downloaded successfully." "green" "[SUCCESS]"
     
-    cd $TARGET_DIR
-
-    cd scripts
-
-    if [ $? -eq 0 ]; then
-        print_fancy "Successfully changed to /scripts directory." "green" "[SUCCESS]"
+    print_fancy "Extracting ZIP file..." "yellow" "[INFO]"
+    if unzip -q "$ZIP_FILE" -d "$TARGET_DIR"; then
+        print_fancy "ZIP file extracted successfully." "green" "[SUCCESS]"
         
-        if [ -f "setup.sh" ]; then
-            print_fancy "Executing setup.sh..." "yellow" "[INFO]"
-            bash setup.sh
-            if [ $? -eq 0 ]; then
-                print_fancy "setup.sh executed successfully." "green" "[SUCCESS]"
+        rm "$ZIP_FILE"
+        print_fancy "ZIP file removed." "green" "[INFO]"
+        
+        cd "$TARGET_DIR"/*
+        
+        if [ -d "scripts" ]; then
+            cd scripts
+            print_fancy "Changed to scripts directory." "green" "[SUCCESS]"
+            
+            if [ -f "setup.sh" ]; then
+                print_fancy "Executing setup.sh..." "yellow" "[INFO]"
+                bash setup.sh
+                if [ $? -eq 0 ]; then
+                    print_fancy "setup.sh executed successfully." "green" "[SUCCESS]"
+                else
+                    print_fancy "Error executing setup.sh." "red" "[ERROR]"
+                    exit 1
+                fi
             else
-                print_fancy "Error executing setup.sh." "red" "[ERROR]"
+                print_fancy "Error: setup.sh not found." "red" "[ERROR]"
                 exit 1
             fi
         else
-            print_fancy "Error: setup.sh not found." "red" "[ERROR]"
+            print_fancy "Error: scripts directory not found." "red" "[ERROR]"
             exit 1
         fi
     else
-        print_fancy "Error: Could not change to /scripts directory." "red" "[ERROR]"
+        print_fancy "Error extracting ZIP file." "red" "[ERROR]"
         exit 1
     fi
 else
-    print_fancy "Error cloning the repository." "red" "[ERROR]"
+    print_fancy "Error downloading repository ZIP." "red" "[ERROR]"
     exit 1
 fi
